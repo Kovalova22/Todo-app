4da33bcaece0c10408edd83f247f49a7213139f3
Iryna Kovalova <71442762+Kovalova22@users.noreply.github.com>
2021-02-05 21:15:05 +0200

    todo server

---
+++ server/server/controllers/todos.js
@@ -0,0 +1,36 @@
+const Todo = require('../models').Todo;
+
+module.exports = {
+  create(req, res) {
+    return Todo
+      .create({
+        title: req.body.title
+      })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+
+  list(req, res) {
+    return Todo
+      .findAll({})
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Todo
+      .findByPk(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+};
